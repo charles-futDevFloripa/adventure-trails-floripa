54da081f0b46623170151e2297e0ba53ea5fd82b
cbiveu <cbiveu@gmail.com>
2024-04-21 23:57:20 -0300

    componente preparado para receber os props + adcionado e configurado propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import CardTrilha from './components/CardTrilha';
 function App() {
-  return;
-  <>
-    <CardTrilha />
-  </>;
-}
+  const listaTrilhas = [
+    {
+      nomeTrilha: 'Trilha da Costa da Lagoa',
+      cidade: 'Floripa',
+      estado: 'SC',
+      duracao: 120,
+      trajeto: 4,
+      dificuldade: 'Iniciante',
+      tipo: 'caminhada / trekking',
+      nomeUusario: 'João Ninguem',
+      urlImagem:
+        'https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg',
+    },
+  ];
 
+  return (
+    <>
+      {listaTrilhas.map((trilha, index) => (
+        <CardTrilha dadosTrilha={trilha} key={index} />
+      ))}
+    </>
+  );
+}
 export default App;
--- src/components/CardTrilha/index.jsx
+++ src/components/CardTrilha/index.jsx
@@ -1,4 +1,28 @@
-function CardTrilha() {
-  return <>CardTrilha</>;
+import * as PropTypes from 'prop-types';
+function CardTrilha({ dadosTrilha }) {
+  return (
+    <>
+      <h1>{dadosTrilha.nomeTrilha}</h1>
+      <span>
+        {dadosTrilha.cidade} / {dadosTrilha.estado}
+      </span>
+      <img src={dadosTrilha.urlImagem} height={300} />
+    </>
+  );
 }
+
+CardTrilha.propTypes = {
+  dadosTrilha: PropTypes.exact({
+    nomeTrilha: PropTypes.string.isRequired,
+    cidade: PropTypes.string.isRequired,
+    estado: PropTypes.string.isRequired,
+    duracao: PropTypes.number.isRequired,
+    trajeto: PropTypes.number.isRequired,
+    dificuldade: PropTypes.string.isRequired,
+    tipo: PropTypes.string.isRequired,
+    nomeUusario: PropTypes.string.isRequired,
+    urlImagem: PropTypes.string.isRequired,
+  }),
+};
+
 export default CardTrilha;
